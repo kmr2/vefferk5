ef968d5cd85b2ad5e6cd1c7f4e9ca6d18cf020fb
unknown <kristofermani@live.com>
2020-04-03 02:12:13 +0000

    laga

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
-from pyrebase import pyrebase
+from flask import Flask, render_template, request, session, redirect, url_for
+import pyrebase
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'covid_19'
 
-# tengin við firebase realtime database á firebase.google.com ( db hjá danielsimongalvez@gmail.com )
 config = {
-    "apiKey":"AIzaSyB6L9PLOIa-y8spupA2UFiegQGN7gmp12E",
+
+    "apiKey": "AIzaSyB6L9PLOIa-y8spupA2UFiegQGN7gmp12E",
     "authDomain": "vefferk5.firebaseapp.com",
     "databaseURL": "https://vefferk5.firebaseio.com",
     "projectId": "vefferk5",
@@ -13,7 +14,6 @@ config = {
     "messagingSenderId": "683227377425",
     "appId": "1:683227377425:web:1317e8ebac70100ee126b0",
     "measurementId": "G-0BBSXPYZF8"
-
 
 }
 
@@ -23,12 +23,71 @@ db = fb.database()
 # Test route til að setja gögn í db
 @app.route('/')
 def index():
-    return "Skrifað í grunn"
+    return render_template("index.html")
 
 # Test route til að sækja öll gögn úr db
-@app.route('/lesa')
-def lesa():
-    return "Lesum úr grunni"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    login = False
+    if request.method == 'POST':
+
+        notendanafn = request.form['uname']
+        lykilorð = request.form['psw']
+
+        u = db.child("notandi").get().val()
+        lst = list(u.items())
+        for i in lst:
+            if notendanafn == i[1]['notendanafn'] and lykilorð == i[1]['lykilorð']:
+                login = True
+                break
+
+        if login:
+            session['logged_in'] = notendanafn
+            return redirect("/topsecret")
+        else:
+            return render_template("nologin.html")
+
+    else:
+        return render_template("no_method.html")
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+# Test route til að sækja öll gögn úr db
+@app.route('/doregister', methods=['GET', 'POST'])
+def doregister():
+    usernames = []
+    if request.method == 'POST':
+
+        notendanafn = request.form['uname']
+        lykilorð = request.form['psw']
+
+        u = db.child("notandi").get().val()
+        lst = list(u.items())
+        for i in lst:
+            usernames.append(i[1]['notendanafn'])
+
+        if notendanafn not in usernames:
+            db.child("notandi").push({"notendanafn": notendanafn, "lykilorð": lykilorð})
+            return render_template("registered.html")
+        else:
+            return render_template("userexists.html")
+
+
+@app.route('/logout')
+def logout():
+    session.pop("logged_in", None)
+    return render_template("index.html")
+
+
+@app.route('/topsecret')
+def topsecret():
+    if 'logged_in' in session:
+        return render_template("topsecret.html")
+    else:
+        return redirect("/")
 
 if __name__ == "__main__":
 	app.run(debug=True)
